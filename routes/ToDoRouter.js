77075d11dc555484523e21f5b1d26f7870e2751a
Davit2020 <dav.eghiazaryan@bk.ru>
2021-02-25 18:12:54 +0400

    add edit button

---
+++ routes/ToDoRouter.js
@@ -0,0 +1,23 @@
+const { getToDos, addnewToDo, deleteToDo,editToDo } = require('../controllers/ToDoController');
+
+const express = require('express');
+const router = express.Router();
+
+
+
+/* GET todeos. */
+router.get('/',getToDos);
+
+/* Add todeo. */
+
+router.post('/',addnewToDo);
+
+
+/* Add todeo. */
+
+router.delete('/:id',deleteToDo);
+/* Edit todeo. */
+
+router.post('/edit',editToDo);
+
+module.exports = router;
